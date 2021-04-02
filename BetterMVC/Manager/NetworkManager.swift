6285b2de73033497ce43d93c82a0633b6d66f288
Joo Hee Kim <imjhk03@gmail.com>
2021-04-02 09:15:49 +0900

    Add empty network manager

---
+++ BetterMVC/Manager/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  BetterMVC
+//
+//  Created by Joo Hee Kim on 21. 04. 02..
+//
+
+import Foundation
+
+final class NetworkManager {
+    
+    static let shared       = NetworkManager()
+    
+}
