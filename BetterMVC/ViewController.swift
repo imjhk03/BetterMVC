3ee775117d021252b1918af74a4b555a73d43f89
Joo Hee Kim <imjhk03@gmail.com>
2021-04-02 09:01:07 +0900

    Initial Commit

---
+++ BetterMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BetterMVC
+//
+//  Created by Joo Hee Kim on 21. 04. 02..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
