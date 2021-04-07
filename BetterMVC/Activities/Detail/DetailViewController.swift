15b5e0bb41d44c2e929fc55d6fe79d8f9b25dae7
Joo Hee Kim <imjhk03@gmail.com>
2021-04-07 10:51:21 +0900

    Add Detail Group and view controller, Add Utilities Group

---
+++ BetterMVC/Activities/Detail/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  BetterMVC
+//
+//  Created by Joo Hee Kim on 21. 04. 07..
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
