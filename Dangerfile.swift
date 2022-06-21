8312e139069085415755f4967975af1a9e59622c
kimjh3 <imjhk03@gmail.com>
2022-06-21 23:30:53 +0900

    Update Dangerfile.swift

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,16 +1,36 @@
 import Danger
+import DangerSwiftLint
 
 let danger = Danger()
 
-// Pull out the edited files
+// Pull out the edited files and find ones that come from a sub-folder
+// where our app lives
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
-let hasChangelogEntry = danger.git.modifiedFiles.contains("CHANGELOG.md")
+let editedAppFiles = editedFiles.filter { $0.contains("/App") }
 
-// Example from danger
 // Let people bail from the CHANGELOG check
-let hasSkipChangelog = danger.github.pullRequest.body.contains("#no_changelog")
+let hasSkipChangelog = danger.github.pullRequest.body?.contains("#no_changelog")
+let hasSkipChangelogLabel = danger.github.issue.labels.first { $0.name == "Skip Changelog" }
+let skipCheck = hasSkipChangelog ?? false || (hasSkipChangelogLabel != nil)
 
 // Request for a CHANGELOG entry with each app change
-if editedFiles.count > 0 && !hasChangelogEntry && !hasSkipChangelog {
-    fail("Please add a CHANGELOG entry for these changes. If you would like to skip this check, add `#no_changelog` to the PR body and re-run CI.")
+if editedAppFiles.count > 0 && !skipCheck {
+  fail("Please add a CHANGELOG entry for these changes. If you would like to skip this check, add `#no_changelog` to the PR body and re-run CI.")
 }
+
+SwiftLint.lint()
+
+// Instead of making a markdown table in the main message
+// sprinkle those comments inline, this can be a bit noisy
+// but it definitely feels magical.
+SwiftLint.lint(inline: true)
+
+// Have different runs of SwiftLint against different sub-folders
+SwiftLint.lint(directory: "Sources", configFile: ".swiftlint.yml")
+SwiftLint.lint(directory: "Tests", configFile: "Tests/HarveyTests/.swiftlint.yml")
+
+// The equivalent to running `swiftlint` in the root of the folder
+SwiftLint.lint(lintAllFiles: true)
+
+// Use a different path for SwiftLint
+SwiftLint.lint(swiftlintPath: "Pods/SwiftLint/swiftlint")
