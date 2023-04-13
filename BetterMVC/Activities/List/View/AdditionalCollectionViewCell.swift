2ec67353e95c88c456f39028ae07bd851c6f311a
Joohee Kim <imjhk03@gmail.com>
2023-04-13 18:53:59 +0900

    테스트용 cell 생성

---
+++ BetterMVC/Activities/List/View/AdditionalCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  AdditionalCollectionViewCell.swift
+//  BetterMVC
+//
+//  Created by Joohee Kim on 2023/04/13.
+//
+
+import UIKit
+
+class AdditionalCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
